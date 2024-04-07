816cd52b4504d4d30059a0249180d61becd99e7a
ErikWe <erikwestberg7@gmail.com>
2024-04-07 13:49:10 +0200

    Better explanation of void-delegate IMappedArgumentRecorderFactory

--- src/Attribinter.Mappers.Abstractions/IVoidDelegateMappedArgumentRecorderFactory.cs
+++ src/Attribinter.Mappers.Abstractions/IVoidDelegateMappedArgumentRecorderFactory.cs
@@ -5,7 +5,7 @@ using System;
 /// <summary>Handles creation of <see cref="IMappedArgumentRecorder{TRecord, TData}"/> using <see langword="void"/>-returning delegates.</summary>
 public interface IVoidDelegateMappedArgumentRecorderFactory
 {
-    /// <summary>Creates a <see cref="IMappedArgumentRecorder{TRecord, TData}"/> using the provided delegate.</summary>
+    /// <summary>Creates a <see cref="IMappedArgumentRecorder{TRecord, TData}"/> using the provided delegate. The created recorder will always return <see langword="true"/>.</summary>
     /// <typeparam name="TRecord">The type of the data record to which data is recorded.</typeparam>
     /// <typeparam name="TData">The type of the recorded data.</typeparam>
     /// <param name="recorderDelegate">The delegate reponsible for recording data.</param>
