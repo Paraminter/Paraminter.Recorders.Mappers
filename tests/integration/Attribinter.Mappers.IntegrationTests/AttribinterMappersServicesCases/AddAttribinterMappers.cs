f8db516aeb10b581d8cc91aa019448f0183349e9
ErikWe <erikwestberg7@gmail.com>
2024-04-21 23:49:22 +0200

    Remove redundant local func

--- tests/integration/Attribinter.Mappers.IntegrationTests/AttribinterMappersServicesCases/AddAttribinterMappers.cs
+++ tests/integration/Attribinter.Mappers.IntegrationTests/AttribinterMappersServicesCases/AddAttribinterMappers.cs
@@ -51,14 +51,12 @@ public sealed class AddAttribinterMappers
     {
         HostBuilder host = new();
 
-        host.ConfigureServices(configureServices);
+        host.ConfigureServices(static (services) => Target(services));
 
         var serviceProvider = host.Build().Services;
 
         var result = serviceProvider.GetRequiredService<TService>();
 
         Assert.NotNull(result);
-
-        static void configureServices(IServiceCollection services) => Target(services);
     }
 }
